783c60e893f8fbd3f351781a4c854117a164165f
David CLERY <clery.dav@gmail.com>
2025-07-30 09:57:03 +0100

    Modif

---
+++ src/Exercice4i2.java
@@ -0,0 +1,5 @@
+public class Exercice4i2 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Exercice4i4.java
@@ -0,0 +1,14 @@
+public class Exercice4i4 {
+    public static void main(String[] args) {
+
+        int i;
+        int iMax;
+        int j;
+        int jMax;
+        int T[12.8];
+
+        iMax = 1:
+        jMax = 1;
+
+    }
+}
